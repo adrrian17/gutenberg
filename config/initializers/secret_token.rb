8b65ecdc79d362b25ac7dd0e0526a6f734674f96
Daniel Martinez <eduardodaniel2@gmail.com>
2013-06-29 08:53:58 -0500

    Commit inicial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Gutenberg::Application.config.secret_key_base = '864f28ab6ef2d8e0d91f8e3aeb74e864dfe1718923f82c0d61c0e7446eaf1a41c7387900dea11060391d8136876680b1d2f61f4bb1db9746053cfa4a8f567732'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Gutenberg::Application.config.session_store :cookie_store, key: '_gutenberg_session'
