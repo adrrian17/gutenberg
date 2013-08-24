1524f848f919b09daa61ef75212e3b7cc1e8322a
Daniel Martinez <eduardodaniel2@gmail.com>
2013-08-24 12:35:52 -0500

    Asegura la propiedad "item_types_id" al modelo de revista

--- app/models/magazine.rb
+++ app/models/magazine.rb
@@ -1,6 +1,8 @@
 class Magazine < ActiveRecord::Base
-	validates :copies, :published_date, :title, :volume, presence: true
-	validates :copies, numericality: { only_integer: true }
-	belongs_to :item_types
-	has_many :loans, foreign_key: :item_id
+  validates :copies, :published_date, :title, :volume, presence: true
+  validates :copies, numericality: { only_integer: true }
+  belongs_to :item_types
+  has_many :loans, foreign_key: :item_id
+
+  before_save { |magazine| magazine.item_types_id = 1 }
 end
--- test/models/magazine_test.rb
+++ test/models/magazine_test.rb
@@ -7,4 +7,9 @@ class MagazineTest < ActiveSupport::TestCase
   should validate_presence_of(:copies)
   should belong_to(:item_types)
   should have_many(:loans)
+
+  def test_should_have_item_types_id_set_to_one
+    @magazine = magazines(:one)
+    assert_equal 1, @magazine.item_types_id
+  end  
 end
