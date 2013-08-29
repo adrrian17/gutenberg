f267f36c3c501290338dbb4f988d6f08a279fea9
Adrian Ayala <adrian.ayala17@gmail.com>
2013-08-28 20:48:00 -0500

    Añade al modelo Magazine una validacion en el campo Copies
    Se añade la validacion para que el campo Copies sea mayor a 0, se incluyen los tests correspondientes

--- app/models/magazine.rb
+++ app/models/magazine.rb
@@ -1,6 +1,6 @@
 class Magazine < ActiveRecord::Base
 	validates :copies, :published_date, :title, :volume, presence: true
-	validates :copies, numericality: { only_integer: true }
+	validates :copies, numericality: { only_integer: true, greater_than: 0 }
 
   belongs_to :category
 	belongs_to :item_types
