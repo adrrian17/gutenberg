c08af9bf12c1805222bb05e273bc518230b78375
Adrian Ayala <adrian.ayala17@gmail.com>
2013-12-05 16:56:45 -0600

    Agrega la nueva lista de categorias por orden alfabetico

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -4,7 +4,7 @@ class CategoriesController < ApplicationController
   # GET /categories
   # GET /categories.json
   def index
-    @categories = Category.all
+    @categories = Category.all.group_by{|c| c.category[0]}
   end
 
   # GET /categories/1
