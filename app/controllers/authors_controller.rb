f91a20569440ba0b59ace430011bb3d312f9d8a0
Adrian Ayala <adrian.ayala17@gmail.com>
2013-12-06 17:29:17 -0600

    Agrega el nuevo marcado y estilos de la lista de autores
    Se elimina el archivo _authors_shelf.html.haml para incluirlo dentro
    del index.html.haml

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -4,7 +4,7 @@ class AuthorsController < ApplicationController
   # GET /authors
   # GET /authors.json
   def index
-    @authors = Author.all
+    @authors = Author.all.group_by{|a| a.full_name[0]}
     @sb_title = 'Autores'
   end
 
