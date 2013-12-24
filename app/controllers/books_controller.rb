c4866d251278906b162c9b65b92e0997caab6f23
Adrian Ayala <adrian.ayala17@gmail.com>
2013-12-24 11:19:03 -0600

    Agrega el marcado y los estilos para la pagina individual de cada libro

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -12,6 +12,7 @@ class BooksController < ApplicationController
   # GET /books/1.json
   def show
     @book = Book.find_by_slug(params[:id])
+    @sb_title = @book.title
   end
 
   # GET /books/new
